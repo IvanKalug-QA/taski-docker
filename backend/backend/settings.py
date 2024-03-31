63f794ba43eb3a3b5ea6e18156d7a09dd7393244
IVAN-GUY <kaluginivan2002@mail.ru>
2024-03-31 22:04:33 +0300

    Add project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.23.30', 'localhost', '127.0.0.1', 'taskikaluginivan.zapto.org']
+ALLOWED_HOSTS = ['51.250.23.30', 'localhost', '127.0.0.1', 'kaluginivantaski.zapto.org']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
